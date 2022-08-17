11d69ca8b009c3784c5a13b7cb526fb4d548c0dc
Pedro Vargas <pedroh_vargas@hotmail.com>
2022-08-17 02:30:25 -0300

    Run build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
 import './App.css';
 
+import {useState, useEffect} from 'react';
+
 function App() {
+  
+
+  const [tarefas, setarTarefas] = useState([
+
+  ]);
+
+  const [modal, setModal] = useState(false);
+
+  const salvarTarefa = () => {
+    var tarefa = document.getElementById('content-tarefa');
+
+    setarTarefas([
+      ...tarefas,
+      {
+        id: new Date().getTime(),
+        tarefa: tarefa.value,
+        finalizada: false
+      }
+    ]);
+
+    window.localStorage.setItem('tarefas',JSON.stringify([
+      ...tarefas,
+      {
+        id: new Date().getTime(),
+        tarefa: tarefa.value,
+        finalizada: false
+      }
+    ]));
+
+    setModal(!modal);
+  };
+
+  const marcarConcluida = (id) => {
+    let novasTarefas = tarefas.filter(function(val){
+        if(val.id == id){
+          val.finalizada = !val.finalizada;
+        }
+        return val;
+    })
+
+    setarTarefas(novasTarefas)
+    window.localStorage.setItem('tarefas',JSON.stringify(novasTarefas));
+  }
+
+  const abrirModal = () => {
+        setModal(!modal);
+  };
+
+  useEffect(() =>{
+    //Fazer uma chamada para API e preencher o estado tarefas.
+    if(window.localStorage.getItem('tarefas',) != undefined){
+      setarTarefas(JSON.parse(window.localStorage.getItem('tarefas')));
+    }
+  },[])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        modal?
+        <div className='modal'>
+          <div className='modalContent'>
+            <h3>Adicionar sua tarefa</h3>
+            <input id="content-tarefa" type="text" />
+            <button onClick={()=>salvarTarefa()}>Salvar!</button>
+          </div>
+        </div>
+        :
+        <div></div>
+      }
+      <div onClick={()=>abrirModal()} className="addTarefa">+</div>
+      <div className='boxTarefas'>
+        <h2>Minhas Tarefas do Dia!</h2>
+        {
+          tarefas.map((val) => {
+            if(!val.finalizada){
+              return (
+                <p onClick={()=>marcarConcluida(val.id)}>{val.tarefa}</p>
+              );
+            }else{
+              return(
+                <p onClick={()=>marcarConcluida(val.id)} style={{textDecoration: 'line-through'}}>{val.tarefa}</p>
+              );
+            }
+          })
+        }
+      </div>
     </div>
   );
 }
